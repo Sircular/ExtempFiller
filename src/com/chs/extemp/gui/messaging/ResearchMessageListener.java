230e2ec0c4f0516b621dbba2a6ba4f0c842b2b53
Sircular <sircular17@gmail.com>
2014-03-03 17:46:53 -0700

    ARGH COMMITS ARE HARD

--- src/com/chs/extemp/gui/messaging/ResearchMessageListener.java
+++
@@ -1,7 +0,0 @@
-package com.chs.extemp.gui.messaging;
-
-import java.util.EventListener;
-
-public interface ResearchMessageListener extends EventListener{
-	void handleMessageEvent(ResearchMessage e);
-}
