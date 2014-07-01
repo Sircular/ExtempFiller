b45f436b483205f9b0714aa0d04a5a242cb3db40
Sircular <Sircular@github.com>
2014-06-30 21:16:27 -0600

    Merged ResearchMenuItem.java into ResearchMenuBar.java.

--- src/com/chs/extemp/gui/menu/ResearchMenuBar.java
+++ src/com/chs/extemp/gui/menu/ResearchMenuBar.java
@@ -1,5 +1,10 @@
 package com.chs.extemp.gui.menu;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
 import com.chs.extemp.gui.ResearchGUI;
 
 import javax.swing.*;
@@ -57,5 +62,38 @@ public class ResearchMenuBar extends JMenuBar {
 
 	public void setContentsEnabled(boolean state) {
 		fileMenu.setEnabled(state);
+	}
+}
+
+@SuppressWarnings("serial")
+class ResearchMenuItem extends JMenuItem {
+	private final Runnable function;
+
+	public ResearchMenuItem(final String name, final Runnable function) {
+		super(name);
+		this.function = function;
+		init();
+	}
+
+	private void init() {
+		addActionListener(new ResearchMenuItemListener(function));
+	}
+
+}
+
+class ResearchMenuItemListener implements ActionListener, ItemListener {
+	private final Runnable function;
+
+	public ResearchMenuItemListener(final Runnable function) {
+		this.function = function;
+	}
+
+	@Override
+	public void actionPerformed(final ActionEvent e) {
+		function.run();
+	}
+
+	@Override
+	public void itemStateChanged(final ItemEvent e) {
 	}
 }
