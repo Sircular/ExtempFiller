07ac197744e9a99d2df4623717592a52a672d7f9
Sircular <sircular17@gmail.com>
2015-01-19 19:48:39 -0700

    Added (untested) partially fixed CLI. (That previous sentence is almost unintelligible.)

--- src/com/chs/extemp/cli/CLI.java
+++ src/com/chs/extemp/cli/CLI.java
@@ -1,5 +1,7 @@
 package com.chs.extemp.cli;
 
+import java.io.File;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,14 +18,35 @@ public class CLI {
 		logger = ExtempLogger.getLogger();
 		logger.addHandler(new CLILogHandler());
 		logger.info("Starting CLI using questions file: " + topicsFilePath);
+		
+		Scanner input = new Scanner(System.in);
 
+		String devKey = "";
 		// select the auth token (unfinished)
-		logger.config("Do you want to use the default Evernote account? [y/N]");
+		if (new File(DataReader.DEFAULT_DEV_KEY_PATH).exists()) {
+			while (true) {
+				logger.config("Do you want to use the default Evernote account? [y/N]");
+				String choice = input.next().trim();
+				if (choice.equals("") || choice.toLowerCase().charAt(0) == 'n') {
+					break;
+				} else if (choice.toLowerCase().charAt(0) == 'y') {
+					devKey = DataReader.loadDevKey(DataReader.DEFAULT_DEV_KEY_PATH);
+					break;
+				} else {
+					logger.warning("Invalid entry. Please try again.");
+				}
+			}
+		}
+		if (devKey.equals("")) {
+			logger.config("Please enter the dev key:");
+			devKey = input.next().trim();
+		}
+		
+		input.close();
 
 		//Start research
 		try {
-			// will soon implement option for custom auth token
-			researcher = new Researcher(DataReader.loadDevKey(DataReader.DEFAULT_DEV_KEY_PATH));
+			researcher = new Researcher(devKey);
 			doResearch(topicsFilePath);
 		} catch (final Exception e) {
 			logger.log(Level.SEVERE, "", e);
